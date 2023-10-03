dd2439d416c3150d8b8a653fd558067f493e1d05
ndkhuong24 <kn134646@gmail.com>
2023-10-03 13:08:40 +0700

    Update-Database

---
+++ Database/ProductDetail.sql
@@ -0,0 +1,20 @@
+﻿--PROCEDURE GET PRODUCT DETAIL
+CREATE PROCEDURE GetProductDetail
+    @Id INT = NULL
+AS
+BEGIN
+    -- Nếu tham số @Id không null, lấy bản ghi theo ID
+    IF @Id IS NOT NULL
+    BEGIN
+        SELECT * FROM ProductDetail WHERE id = @Id;
+    END
+    ELSE
+    -- Ngược lại, trả về tất cả các bản ghi
+    BEGIN
+        SELECT PD.id,PD.category_id FROM ProductDetail AS PD JOIN Category AS C ON PD.category_id=C.id
+    END
+END;
+
+select *from ProductDetail
+select*from Category
+select*from Style
