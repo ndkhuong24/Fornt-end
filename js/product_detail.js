9dbf2085ed1b376b100b376839228e23adeb3fee
DESKTOP-O564RM8\alfon <alfonsominh0512@gmail.com>
2023-11-09 11:06:07 +0700

    sửa product_detail

--- js/product_detail.js
+++ js/product_detail.js
@@ -1,10 +1,20 @@
 const urlParams = new URLSearchParams(window.location.search);
 const itemId = urlParams.get("id");
-function fetchdata(){
-    fetch(`http://localhost:8080/api/ProductDetail/`+itemId)
-    .then((response)=>response.json())
-    .then((data)=>{
-        console.log(data);
-    })
+
+function fetchdata() {
+  fetch(`http://localhost:8080/api/ProductDetail/` + itemId)
+    .then((response) => response.json())
+    .then((data) => {
+      const row = document.getElementById("addtocart");
+      const quantity = document.getElementById("quantity").value;
+
+     
+
+      row.innerHTML +=
+        `
+         <p class="addtocart"><a href="#" class="btn btn-primary btn-addtocart" onclick="cart.add(${data.id}, ${quantity})"><i
+         class="icon-shopping-cart"></i> Add to Cart</a></p>`;
+    });
 }
+
 fetchdata();
