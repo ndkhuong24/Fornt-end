539e7c3ca11381d14125d30c43ddd25cb0d13832
ndkhuong24 <kn134646@gmail.com>
2023-11-29 16:30:31 +0700

    no message

--- js/product_detail.js
+++ js/product_detail.js
@@ -146,3 +146,66 @@ function fetchdata() {
 }
 
 fetchdata();
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
+
+function clearAllCookies() {
+  const cookies = document.cookie.split(";");
+  for (const cookie of cookies) {
+    const eqPos = cookie.indexOf("=");
+    const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  }
+}
+
+const cookie = getCookie("token-user");
+
+if (cookie != null && cookie != "") {
+  document.getElementById("dangKiOption").classList.add("hidden");
+  document.getElementById("dividerDangNhapOption").classList.add("hidden");
+  document.getElementById("dangNhapOption").classList.add("hidden");
+
+  document.getElementById("thongTinOption").classList.remove("hidden");
+  document.getElementById("dividerThongTinOption").classList.remove("hidden");
+  document.getElementById("dangXuatOption").classList.remove("hidden");
+}
+
+const userData = localStorage.getItem("userData-customer");
+if (userData) {
+  document.getElementById("fullname").innerText = userData;
+}
+
+document.getElementById("logoutButton").addEventListener("click", function () {
+  if (cookie) {
+    fetch("http://localhost:8080/api/auth/logout", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${cookie}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.message === "You've been signed out!") {
+          clearAllCookies();
+
+          var cookieNow = getCookie("cookie");
+          if (cookieNow === undefined) {
+            window.location.href = "/login.html";
+          }
+
+          localStorage.removeItem("userData-customer");
+          localStorage.removeItem("data-customer");
+        } else {
+          console.error("Lỗi khi đăng xuất:");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        console.error("Đăng xuất thất bại");
+      });
+  }
+});
